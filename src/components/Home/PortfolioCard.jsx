6c828bf79b9bd91e8671d3a551197a3f3c7c75c4
Nuhan Alam <nuhanalam@gmail.com>
2025-11-20 14:38:31 +0600

    Github Link added on portfolio card

--- src/components/Home/PortfolioCard.jsx
+++ src/components/Home/PortfolioCard.jsx
@@ -1,6 +1,7 @@
 import { FaLinkedin } from "react-icons/fa";
 import { IoLogoYoutube } from "react-icons/io";
 import { SiLeetcode } from "react-icons/si";
+import { FiGithub } from "react-icons/fi";
 
 const PortfolioCard = () => {
     return (
@@ -84,16 +85,15 @@ const PortfolioCard = () => {
             <SiLeetcode className="w-6" />
           </a>
         </li>
+        {/* GitHub */}
         <li>
           <a 
-            href="https://www.facebook.com/Nu7an" 
+            href="https://github.com/Nuhan-Alam" 
             target="_blank" 
             rel="noopener noreferrer"
             className="w-10 h-10 text-white hover:text-black flex items-center justify-center border border-gray-300 rounded-full hover:bg-gray-100 transition-colors"
           >
-            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-            </svg>
+            <FiGithub className="w-8"/>
           </a>
         </li>
       </ul>
